4f82e3926e7f193df6653a11df2336354b0a565b
Carlos Monterrosa <cagmonterrosa@indracompany.com>
2020-08-06 19:56:25 -0500

    MrMonterrosa
    se implementa el uso de la linterna y ademas deja la maqueta para la creacion del zoom

--- js/custom2.js
+++ js/custom2.js
@@ -7,14 +7,28 @@ function playSound() {
     sound.play();
 }
 
+var streaming;
+var trackStream;
+var _torch = false;
+
+function linterna() {
+    console.log(_torch);
+    _torch = !_torch;
+    trackStream.applyConstraints({
+        advanced: [{torch: _torch}]
+    })
+}
+
 window.addEventListener('load', function () {
     let selectedDeviceId;
-    const codeReader = new ZXing.BrowserMultiFormatReader()
+    const codeReader = new ZXing.BrowserMultiFormatReader();
     console.log('ZXing code reader initialized')
     codeReader.listVideoInputDevices()
       .then((videoInputDevices) => {
         const sourceSelect = document.getElementById('sourceSelect')
         selectedDeviceId = videoInputDevices[0].deviceId
+        console.log("selectedDeviceId", videoInputDevices);
+        
         if (videoInputDevices.length >= 1) {
           videoInputDevices.forEach((element) => {
             const sourceOption = document.createElement('option')
@@ -29,27 +43,48 @@ window.addEventListener('load', function () {
           };
 
           const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-          sourceSelectPanel.style.display = 'block'
+          sourceSelectPanel.style.display = 'block'          
         }
 
+        
+
         document.getElementById('startButton').addEventListener('click', () => {
-          codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-            if (result) {
-              console.log(result);
-              playSound();
-              document.getElementById('result').value = result.text
-              codeReader.reset();
-            }
-            if (err && !(err instanceof ZXing.NotFoundException)) {
-              console.error(err)
-              //document.getElementById('result').value = err
-            }
-          })
+
+            navigator.mediaDevices.getUserMedia({
+                video: {
+                  deviceId: selectedDeviceId
+                }
+            }).then(stream => {
+                streaming = stream;
+                trackStream = stream.getVideoTracks()[0];
+                console.log(trackStream);
+                const torchBtn = document.querySelector('#torchButton');
+                torchBtn.addEventListener('click', () => {
+                    linterna();
+                });
+            });
+
+            codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+                if (result) {
+                    console.log(result);
+                    playSound();
+                    document.getElementById('result').value = result.text
+                    linterna();
+                    codeReader.reset();
+                    trackStream.stop();
+                }
+                if (err && !(err instanceof ZXing.NotFoundException)) {
+                    console.error(err)
+                }
+            })
           console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
         })
 
+        document.getElementById('startButton').click();
+
         document.getElementById('resetButton').addEventListener('click', () => {
-          codeReader.reset()
+          codeReader.reset();
+          trackStream.stop();
           document.getElementById('result').value = '';
           console.log('Reset.')
         })
